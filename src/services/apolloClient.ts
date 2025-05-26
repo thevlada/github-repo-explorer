506d8645870b7d7fe9bdde180584e40185a29944
V <jovanovicv90@gmail.com>
2025-05-26 12:16:47 +0200

    fix: recreate apolloClient with environment variables after history cleanup

---
+++ src/services/apolloClient.ts
@@ -0,0 +1,37 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // GitHub PAT token from environment variables
+  const token = process.env.REACT_APP_GITHUB_TOKEN;
+
+  if (!token) {
+    console.error(
+      'GitHub token not found. Please set REACT_APP_GITHUB_TOKEN in your .env file.'
+    );
+  }
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+export const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      errorPolicy: 'all',
+    },
+    query: {
+      errorPolicy: 'all',
+    },
+  },
+});
